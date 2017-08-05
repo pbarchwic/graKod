200f9784fc99b98d055863038298e216a813c4ac
pbarchwic <barchwicp@gmail.com>
2017-08-05 18:00:31 +0200

    ver. 0.2

--- gra.js
+++ gra.js
@@ -1,67 +1,49 @@
-function namep1()
-{
-    // przypisanie obiektu pola tekstowego do zmiennej
-    var pole_imie = document.forms['player1'].p1;
-    // odczyt imienia
-    var p1 = pole_imie.value;
-    // sprawdzenie czy imie jest wpisane
-if (p1 != '')
-	{
-        document.getElementById('hellop1').innerHTML = "witaj "+ p1 ;
-	}
-    else
-    {
-        alert('Wpisz swoje imię!');
-        pole_imie.focus();
-    }
-    return false;
-}
-function namep2()
-{
-    // przypisanie obiektu pola tekstowego do zmiennej
-    var pole_imie = document.forms['player2'].p2;
-    // odczyt imienia
-    var p2 = pole_imie.value;
-    // sprawdzenie czy imie jest wpisane
-if (p2 != '')
-	{
-    
-	document.getElementById('hellop2').innerHTML = "witaj "+ p2 ;
-	}
-    else
-    {
-        alert('Wpisz swoje imię!');
-        pole_imie.focus();
-    }
-    return false;
-}
-
+// Zmienne globalne
 	var los ;
 	var ruchy = 0;
 	var zgadnij;
 
-	document.getElementById('graj').addEventListener('click', gra);
+// Pobieranie imion graczy
+
+
+function playersName()
+{
+    var fieldPlayer1 = document.forms['player1'].p1;
+    var p1 = fieldPlayer1.value;
 	
+    var fieldPlayer2 = document.forms['player2'].p2;
+    var p2 = fieldPlayer2.value;
+
+if (p1 != '' && p2!='')
+	{
+        document.getElementById('hello').innerHTML = "Witaj Agencie "+ p1 +" oraz Agencie "+ p2;
+	}
+    else
+    {
+        alert('Podaj imiona dwóch agentów!');
+    }
+    return true;
+}
+
+	
+// Wypisywanie odpowiedniego komunikatu
 function gra() {
 		zgadnij = document.getElementById('textbox').value;
     if (zgadnij == los) {
         document.getElementById('wynik').innerHTML = "GRATULACJE";
     } if (zgadnij < los) {
-        document.getElementById('wynik').innerHTML = "za nisko";
+        document.getElementById('wynik').innerHTML = "Kod niepoprawny,za nisko. Niech teraz podejmie probę agent ";
         ruchy++;
     } if (zgadnij > los) {
         document.getElementById('wynik').innerHTML = "za wysoko";
         ruchy++;
-    } else if (zgadnij==="")
+    } else if (zgadnij==="" || typeof zgadnij==='string')
 	{
-        document.getElementById('wynik').innerHTML = "podaj Kod";
+        document.getElementById('wynik').innerHTML = "Wpisz poprawny kod!";
     }
 }
 
-
-document.getElementById('losuj').addEventListener('click', losowanie);
-
-
+//losowanie ukrytej liczby
 
  function losowanie() {
     los = Math.floor((Math.random() * 9000) + 1000);
@@ -69,25 +51,3 @@ document.getElementById('losuj').addEventListener('click', losowanie);
 }	
 
 
-noStroke();
-
-// position of the car
-var x = 10;
-    
-draw = function() {
-    
-    // all lines of code inside here will be run repeatedly
-    background(151, 244, 247);
-    
-    // draw the car body
-    fill(255, 0, 115);
-    rect(x, 200, 100, 20);
-    rect(x + 15, 178, 70, 40);
-    
-    // draw the wheels
-    fill(77, 66, 66);
-    ellipse(x + 25, 221, 24, 24);
-    ellipse(x + 75, 221, 24, 24);
-    
-    x = x + 1;
-};
